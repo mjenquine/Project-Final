8d2d68ee1fcec67aa0759b0c2adf0774885c8aa1
Mark Jenquine <mjenquine@gmail.com>
2020-04-06 17:37:54 -0500

    user auth complete

--- mtb_trail_reviews_api/src/index.js
+++ mtb_trail_reviews_api/src/index.js
@@ -1,16 +1,16 @@
 const { GraphQLServer } = require('graphql-yoga')
 const { prisma } = require('./generated/prisma-client')
+const Query = require('./resolvers/Query')
+const Mutation = require('./resolvers/Mutation')
+const User = require('./resolvers/User')
+const Review = require('./resolvers/Review')
+
 
 const resolvers = {
-
-  Mutation: {
-    post: (root, args, context) => {
-      return context.prisma.createReview({
-        trailName: args.trailName,
-        trailCondition: args.trailCondition,
-      })
-    },
-  },
+  Query,
+  Mutation,
+  User,
+  Link
 }
 
 const server = new GraphQLServer({
