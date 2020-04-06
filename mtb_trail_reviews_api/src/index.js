06da685c1b11570de5f0d37c31961f98997e4203
Mark Jenquine <mjenquine@gmail.com>
2020-04-06 16:48:13 -0500

    added prisma client and user datamodel.prisma

--- mtb_trail_reviews_api/src/index.js
+++ mtb_trail_reviews_api/src/index.js
@@ -1,40 +1,28 @@
 const { GraphQLServer } = require('graphql-yoga')
-
-const typeDefs = `
-type Query {
-  info: String!
-  feed: [Review!]!
-}
-
-type Review {
-  id: ID!
-  trailName: String!
-  trailCondition: String!
-}
-`
-
-let reviews = [{
-  id: 'review-1',
-  trailName: "Hoyt Park",
-  trailCondition: "Muddy in the low spots. Badly in need of sun"
-}]
+const { prisma } = require('./generated/prisma-client')
 
 const resolvers = {
   Query: {
     info: () => 'Trail condition reviews',
-    feed: () => reviews,
+    feed: (root, args, context, info) => {
+      return context.prisma.reviews()
+    },
   },
 
-  Review: {
-    id: (parent) => parent.id,
-    trailName: (parent) => parent.trailName,
-    trailCondition: (parent) => parent.trailCondition,
-  }
+  Mutation: {
+    post: (root, args, context) => {
+      return context.prisma.createReview({
+        trailName: args.trailName,
+        trailCondition: args.trailCondition,
+      })
+    },
+  },
 }
 
 const server = new GraphQLServer({
-  typeDefs,
+  typeDefs: './src/schema.graphql',
   resolvers,
+  context: { prisma },
 })
 
 server.start(() => console.log('server is running on http://localhost:4000'))
