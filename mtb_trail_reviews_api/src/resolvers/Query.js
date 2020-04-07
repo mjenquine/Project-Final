e134eb77862de7094a71440d1f8e95f832288239
Mark Jenquine <mjenquine@gmail.com>
2020-04-07 12:10:03 -0500

    filter added to query and tested

--- mtb_trail_reviews_api/src/resolvers/Query.js
+++ mtb_trail_reviews_api/src/resolvers/Query.js
@@ -1,5 +1,17 @@
-function feed(parent, args, context, info) {
-  return context.prisma.reviews()
+async function feed(parent, args, context) {
+  const where = args.filter
+    ? {
+        OR: [
+          { trailName_contains: args.filter },
+          { trailCondition_contains: args.filter },
+        ],
+      }
+    : {}
+
+  const reviews = await context.prisma.reviews({
+    where
+  })
+  return reviews
 }
 
 module.exports = {
